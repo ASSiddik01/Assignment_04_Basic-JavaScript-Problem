ac0b4437a1d83490da94aa52e80e29277d666fa1
Shama <assiddik001@gmail.com>
2021-08-09 09:58:36 +0600

    problem-2 solved

--- assignment_4.js
+++ assignment_4.js
@@ -1,9 +1,11 @@
-// Problem-1
+// Problem-01
 
 function seerToMon(seer) {
+    // Input type check
     if (typeof seer != "number") {
         return "Please! give a number";
-    } 
+    }
+    // Negative input check
     if (seer < 40) {
         return "Please! give a number greater than 40 or equal to 40";
     }
@@ -13,3 +15,32 @@ function seerToMon(seer) {
 
 const result = seerToMon(800);
 console.log(result);
+
+// Problem-02
+
+function totalSales(shirtQuantity, pantQuantity, shoeQuantity) {
+    // Input type check
+    if (typeof shirtQuantity != "number" || typeof pantQuantity != "number" || typeof shoeQuantity != "number") {
+        return "Please! give all product quantity number.If you don't purchase any quantity of an item, give the value 0";
+    }
+    // Negative input check
+    if (shirtQuantity < 0 || pantQuantity < 0 || shoeQuantity < 0) {
+        return "Negative quantity is not acceptable";
+    }
+    // Per product price
+    const perShirtPrice = 100;
+    const perPantPrice = 200;
+    const perShoePrice = 500;
+
+    // Every item total cost calculation
+    const totalShirtPrice = perShirtPrice * shirtQuantity;
+    const totalPantPrice = perPantPrice * pantQuantity;
+    const totalShoePrice = perShoePrice * shoeQuantity;
+
+    // All item total cost calculation
+    const totalPrice = totalShirtPrice + totalPantPrice + totalShoePrice;
+    return totalPrice
+}
+
+const total = totalSales(2, 0, "-5");
+console.log(total);
